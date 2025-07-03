d05be1fbcea74d290d68f8a5737620c5b3220838
William <williamdrek64@gmail.com>
2025-07-03 17:42:06 +0300

    added sqlite database

--- app.py
+++ app.py
@@ -13,8 +13,17 @@ def hello_world():
     return 'Hello, World!'
  
 # Run the application
+@app.route('/')
+def index():
+    return "Welcome to the homepage!"
+
+@app.route('/about')
+def about():
+    return "This is the about page." 
 
 if __name__ == '__main__':
 
     app.run(debug=True)  # debug=True enables automatic reloader and debugger
  
+    
+
